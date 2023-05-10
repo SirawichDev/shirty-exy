dca9a4901004e73b167f965d6bf188d8983059e0
SirawichDev <sirawit0676@gmail.com>
2023-05-10 12:11:53 +0700

    feat: make shirt backdrop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 'use client'
 import Image from 'next/image'
 import { Canvas, Vector3 } from '@react-three/fiber'
-import { Center, OrbitControls } from '@react-three/drei'
-import { Inter } from 'next/font/google'
-import Shirt from './components/shirt.component'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Center, Environment, OrbitControls } from '@react-three/drei'
+import Shirt from '@/app/components/shirt.component'
+import ShirtBackdrop from '@/app/components/backdrop.component'
 
 export default function Home() {
   const position: Vector3 = [-1, 0, 2.5]
@@ -13,9 +11,12 @@ export default function Home() {
   return (
     <main className="h-[100vh]">
       <span className="text-black">Test</span>
-      <Canvas camera={{ position, fov }} eventPrefix="client">
+      <Canvas shadows camera={{ position, fov }} eventPrefix="client">
+        <ambientLight intensity={0.2} />
+        <Environment preset="sunset" />
         <Center>
           <Shirt />
+          <ShirtBackdrop />
         </Center>
         <OrbitControls />
       </Canvas>
