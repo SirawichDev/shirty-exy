d5f5d97b01ff93058046735356bc68fe496a43e0
SirawichDev <sirawit0676@gmail.com>
2023-05-10 12:58:56 +0700

    feat: camera component imported

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,23 @@ import { Canvas, Vector3 } from '@react-three/fiber'
 import { Center, Environment, OrbitControls } from '@react-three/drei'
 import Shirt from '@/app/components/shirt.component'
 import ShirtBackdrop from '@/app/components/backdrop.component'
+import Camera from './components/camera.component'
 
 export default function Home() {
   const position: Vector3 = [-1, 0, 2.5]
   const fov = 25
   return (
     <main className="h-[100vh]">
-      <span className="text-black">Test</span>
+      <span className="text-black absolute">Test</span>
       <Canvas shadows camera={{ position, fov }} eventPrefix="client">
         <ambientLight intensity={0.2} />
         <Environment preset="sunset" />
-        <Center>
-          <Shirt />
-          <ShirtBackdrop />
-        </Center>
+        <Camera>
+          <Center>
+            <Shirt />
+            <ShirtBackdrop />
+          </Center>
+        </Camera>
         <OrbitControls />
       </Canvas>
     </main>
